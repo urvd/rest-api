32e67946c7123f4396f05668dcffaf30ddefebaa
uriel <uriel.vido@hotmail.fr>
2020-03-06 13:03:11 +0100

    modif config

--- src/main/java/com/app/restapi/config/ExtraConfig.java
+++ src/main/java/com/app/restapi/config/ExtraConfig.java
@@ -3,27 +3,34 @@ package com.app.restapi.config;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import javax.sql.DataSource;
+
 import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.data.web.SpringDataWebAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ExtraConfig {
+	@Value("${spring.datasource.url}")
+    private String dbUrl;
 	@Bean  
-    public BasicDataSource dataSource() throws URISyntaxException {
+    public DataSource dataSource() throws URISyntaxException {
         URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
         String username = dbUri.getUserInfo().split(":")[0];
         String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+        String finalDbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
 
-        BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setUrl(dbUrl);
-        basicDataSource.setUsername(username);
-        basicDataSource.setPassword(password);
-
-        return basicDataSource;
+        return DataSourceBuilder
+                .create()
+                .username(username)
+                .password(password)
+                .url(finalDbUrl)
+                .build();
     }
 
 }
